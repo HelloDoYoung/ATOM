0a11232f454b1426c1bb1731cbffcacf946fa9b4
DoYoung Kim <kimdoyoung_6004@naver.com>
2025-09-09 20:26:54 +0900

    카메라 해상도를 1920x1080으로 변경하고, 빨간색 HSV 범위 필터링 기능 추가 및 결과 이미지 표시

--- 2025.09.09.py
+++ 2025.09.09.py
@@ -4,8 +4,8 @@ import numpy as np
 class CameraOpen:
     def __init__(self):
         self.cap = cv2.VideoCapture(1)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         if not self.cap.isOpened():
             print('failed to open camera')
             exit()
@@ -14,11 +14,20 @@ class CameraOpen:
         gaussian_src = cv2.GaussianBlur(src, (0, 0), sigmaX=2, sigmaY=2)
         return gaussian_src
 
+    def redHsvInRange(self, src):
+        red_lower_bound = np.array([0, 150, 150], dtype=np.uint8)
+        red_upper_bound = np.array([20, 255, 255], dtype=np.uint8)
+        hsv_src = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+        hsv_dst = cv2.inRange(hsv_src, red_lower_bound, red_upper_bound)
+        return hsv_dst
+
     def main(self):
         _, src = self.cap.read()
         gaussian_src = self.gaussian_blur(src)
+        hsv_red_src = self.redHsvInRange(gaussian_src)
         cv2.imshow('src', src)
         cv2.imshow('gaussian_blur', gaussian_src)
+        cv2.imshow('hsv_red_src', hsv_red_src)
 
 if __name__ == '__main__':
     node = CameraOpen()
